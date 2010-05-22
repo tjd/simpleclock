0ef0f42d4f8b1bfe6eec9086af1220b24c9c2872
Toby Donaldson <tjdonaldson@gmail.com>
2010-05-22 15:27:49 -0700

    initial Eclipse setup

---
+++ clock/src/simpleclock/SpinningArrow.java
@@ -0,0 +1,33 @@
+package simpleclock;
+
+import processing.core.PApplet;
+
+public class SpinningArrow extends PApplet {
+
+	public float angle;
+	public float da; // rotation speed
+
+	public void setup() {
+		size(400, 400);
+		angle = 0f;
+		da = 0.025f;
+		stroke(0, 0, 255);
+		fill(0, 0, 255);
+	}
+
+	public void draw() {
+		background(255);
+
+		// Be careful: the order of the next two statement matters
+		translate(width / 2, height / 2);
+		rotate(angle); // rotations are in radians
+
+		// draw the arrow
+		line(0, -height / 4, 0, height / 4);
+		triangle(-5, -height / 4, 5, -height / 4, 0, -height / 4 - 10);
+
+		// update the arrow
+		angle += da;
+	}
+
+}
---
+++ clock/src/simpleclock/TimeTest.java
@@ -0,0 +1,20 @@
+package simpleclock;
+
+import processing.core.PApplet;
+
+public class TimeTest extends PApplet {
+
+	public void setup() {
+		size(150, 150);
+	}
+
+	public void draw() {
+		background(204);
+		int s = second(); // Values from 0 - 59
+		int m = minute(); // Values from 0 - 59
+		int h = hour(); // Values from 0 - 23
+		line(s, 0, s, 33);
+		line(m, 33, m, 66);
+		line(h, 66, h, 100);
+	}
+}
